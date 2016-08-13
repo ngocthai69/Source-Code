b12ac8ff03715dd06aca82d0de71a73ac388049e
Thai Ngoc <ngocthaihn2@gmail.com>
2016-08-13 22:20:08 +0700

    get location by gps

---
+++ GPS LOCATION/GPS_Location/app/src/main/java/kmt/khoan/gpslocation/MainActivity.java
@@ -0,0 +1,53 @@
+package kmt.khoan.gpslocation;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnShowLocation;
+    TextView txtLocation;
+    // GPSTracker class
+    GPSTracker gps;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnShowLocation = (Button) findViewById(R.id.btnShowLocation);
+        txtLocation = (TextView) findViewById(R.id.txtLocation);
+
+        // show location button click event
+        btnShowLocation.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // create class object
+                gps = new GPSTracker(MainActivity.this);
+
+                // check if GPS enabled
+                if(gps.canGetLocation()){
+
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
+
+                    // \n is for new line
+                    Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+                    String location = "https://www.google.com/maps/@"+latitude+","+longitude+",15z";
+                    txtLocation.setText(location);
+                    Log.i("Location", location);
+                }else{
+                    // can't get location
+                    // GPS or Network is not enabled
+                    // Ask user to enable GPS/network in settings
+                    gps.showSettingsAlert();
+                }
+
+            }
+        });
+    }
+}
